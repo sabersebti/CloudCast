1a10aa6ff72b72fae73a7d9cabfaf63fdaaec35a
saber sebti <sebti.saber1@gmail.com>
2023-03-15 13:03:44 +0000

    fixed the api url

--- script.js
+++ script.js
@@ -1,70 +1,69 @@
-// Select elements from HTML
-const searchForm = document.querySelector('#search-form');
-const searchInput = document.querySelector('.weather-search');
-const todaySection = document.querySelector('#today');
-const forecastSection = document.querySelector('#forecast');
-const searchHistory = document.querySelector('.search-history');
+// Get references to the HTML elements
+const searchFormEl = document.querySelector("#search-form");
+const searchInputEl = document.querySelector("#search-input");
+const historyEl = document.querySelector("#history");
+const todayEl = document.querySelector("#today");
+const forecastEl = document.querySelector("#forecast");
 
-// API endpoint
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
-const apiKey = `&appid=b2bbb7276d8739c63b0ae6a20dea7e17`;
+// Initialize the search history array
+let searchHistory = [];
 
-// Search for city weather information
-searchForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const city = searchInput.value;
-  searchInput.value = '';
+// Function to handle the form submission
+function handleFormSubmit(event) {
+  event.preventDefault();
 
-  // Get current weather information
-  fetch(`${apiUrl}${city}${apiKey}`)
-    .then(res => res.json())
-    .then(data => {
-      // Update today section
-      todaySection.innerHTML = `
-        <h2>${data.name} (${formatDate(data.dt * 1000)})</h2>
-        <div class="card-body">
-          <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather icon">
-          <div class="card-text">
-            <p>Temperature: ${data.main.temp} &#8457;</p>
-            <p>Humidity: ${data.main.humidity}%</p>
-            <p>Wind Speed: ${data.wind.speed} mph</p>
-          </div>
-        </div>
-      `;
+  // Get the search input value
+  const searchInput = searchInputEl.value.trim();
 
-      // Get 5-day forecast information
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}${apiKey}`)
-        .then(res => res.json())
-        .then(data => {
-          // Clear previous forecast information
-          forecastSection.innerHTML = '';
+  if (searchInput !== "") {
+    // Call the API to get the weather data
+    getWeatherData(searchInput);
+    // Add the search term to the search history
+    addToSearchHistory(searchInput);
+    // Clear the search input
+    searchInputEl.value = "";
+  }
+}
 
-          // Loop through 5-day forecast
-          for (let i = 0; i < data.list.length; i += 8) {
-            const forecast = data.list[i];
-            const date = formatDate(forecast.dt * 1000);
+// Function to get the weather data for a given city
+function getWeatherData(city) {
+  // Construct the API URL using the city name and API key
+  const apiKey = "6cd376d08d644ada8e8120127231503";
+  const currentWeatherApiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  const forecastApiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5`;
 
-            // Update forecast section
-            forecastSection.innerHTML += `
-              <div class="card">
-                <p>${date}</p>
-                <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="Weather icon">
-                <p>Temperature: ${forecast.main.temp} &#8457;</p>
-                <p>Humidity: ${forecast.main.humidity}%</p>
-              </div>
-            `;
-          }
-        });
-
-      // Add city to search history
-      searchHistory.innerHTML += `<li>${city}</li>`;
+  // Call the API to get the current weather data for the city
+  fetch(currentWeatherApiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Use the retrieved data to update the UI with current weather info
+      console.log(data);
     });
-});
 
-// Format date
-function formatDate(date) {
-  const d = new Date(date);
-  const year = d.
+  // Call the API to get the 5-day forecast for the city
+  fetch(forecastApiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Use the retrieved data to update the UI with 5-day forecast info
+      console.log(data);
+    });
+}
 
+// Function to add a search term to the search history
+function addToSearchHistory(searchTerm) {
+  // Add the search term to the search history array
+  searchHistory.push(searchTerm);
 
-  
+  // Add the search term to the search history element
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = searchTerm;
+  historyEl.appendChild(listItemEl);
+}
+
+// Add event listener to the form element
+searchFormEl.addEventListener("submit", handleFormSubmit);
+
